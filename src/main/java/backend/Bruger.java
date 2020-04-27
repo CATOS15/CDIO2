247a135c5c77216a2cc5d72b20d7ff2bce9730f8
Oliver Ro Møltoft Christensen <bistimi@live.dk>
2020-04-27 15:50:45 +0200

    fjernet noget fra bruger klassen

--- src/main/java/backend/Bruger.java
+++ src/main/java/backend/Bruger.java
@@ -1,13 +1,7 @@
 package backend;
 
-import javax.servlet.ServletContext;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
-import javax.ws.rs.core.Context;
-import javax.ws.rs.core.MediaType;
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 
 @Path("/bruger")
 public class Bruger {
